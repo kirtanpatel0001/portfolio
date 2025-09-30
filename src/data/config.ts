6f8e4ab036f133869ea9c194f1acf96e484764b5
kirtanpatel0001 <kirtan6065@gmail.com>
2025-09-30 20:04:28 +0530

    first commit

---
+++ src/data/config.ts
@@ -0,0 +1,48 @@
+const config = {
+  title: "KIRTAN PATEL",
+  description: {
+    long: "Explore the portfolio of KIRTAN PATEL, a Computer Science Engineer with 1.5 year experience in Devlopment, DevOps & Database management with hands-on experience in deploying & automating workflows, & collaborating with foreign clients. Strong analytical skills, effective communication, and a team-oriented approach to driving process improvements and delivering results.",
+    short:
+      "Discover the portfolio of KIRTAN PATEL, a DATA SCIENTIST with 3  years of experience solving real life problems in DevOps and Development.",
+  },
+  keywords: [
+    "KIRTAN PATEL",
+    "KIRTAN PATEL",
+    "kirtanpatel",
+    "kirtanpatel.com",
+    "KIRTAN PATEL portfolio",
+    "portfolio",
+    "DevOps",
+    "Devlopment",
+    "Database",
+    "AI Docker file optimizer",
+    "Finance me",
+    "creative technologist",
+    "web development",
+    "interactive websites",
+    // "GSAP",
+    // "React",
+    // "Next.js",
+    // "Spline",
+    // "Framer Motion",
+    "AWS",
+    "DevSecOps",
+    "AIOps",
+    "Azure",
+  ],
+  author: "KIRTAN PATEL",
+  email: "kirtanpatel7363@gmail.com",
+  site: "https://kirtanpatel.com",
+
+  get ogImg() {
+    return this.site + "/assets/seo/og-image.png";
+  },
+  social: {
+    twitter: "https://x.com/_KIRTAN_PATEL/",
+    linkedin: "https://www.linkedin.com/in/kirtan-patel/",
+    instagram: "https://www.instagram.com/_.celestial_kirtan._/",
+    facebook: "https://www.facebook.com/profile.php?id=100010029821759",
+    github: "https://github.com/KIRTAN-PATEL/",
+  },
+};
+export { config };
